fb68e59641485d049ea7b2293db99516f5211c09
Drinor Dalipi <dalipi0605@gmail.com>
2021-09-25 04:08:16 +0200

    added particles

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+	<style>
+		.particles-js {
+			z-index: -1 !important;
+		}
+
+	</style>	
+</head>
+<body>
+	<div id="particles-js"></div>
+
+	<div class="content">COntent</div>
+
+
+<script src="{{asset('js/particles.js')}}"></script>
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
